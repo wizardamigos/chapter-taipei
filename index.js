59bd414677e97f507681a78936289947c2f1f432
serapath <dev@serapath.de>
2019-02-27 22:56:08 +0000

    ADD content pages

--- index.js
+++ index.js
@@ -1,132 +1,84 @@
-// WIZARD COLORS
-var dB = '#43409a' // darkBlue
-var b  = '#3022bb' // blue
-var lB = '#6f68ae' // lightBlue
-var lP = '#f989ff' // lightPink
-var dP = '#730d61' // darkPink
-var B  = '#080707' // black
-var g  = '#2e3f41' // grey
-var sY = '#f7da8b' // skinYellow
-var W  = '#ffffff' // white
-var t  = 'rgba(255, 255, 255, .0)'
-// WEBSITE COLORS
-var violet       = '#331e38'
-var grey         = '#a0c1b9'
-var blue         = '#70a0af'
-var lightYellow  = '#f4e8c1'
-var yellow       = '#ffee33'
-// FONTS
-var fontXXS = '10'
-var fontXS  = fontXXS*1.3
-var fontS   = fontXXS*1.6
-var fontM   = fontXXS*2.0
-var fontXM  = fontXXS*2
-var fontXXM = fontXXS*2.2
-var fontL   = fontXXS*3
-var fontXL  = fontXXS*3.8
-var fontXXL = fontXXS*6
-var head = `
-  <title> WizardAmigos Chapter - Taipei </title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-  <style>
-    html          {
-      box-sizing  : border-box;
-      display     : table;
-      min-width   : 100%;
-      margin      : 0;
-      font-size   : 18px;
+const chapterpage = require('chapter-page')
+
+const colors = {
+  color0: '#43409a', // darkBlue,
+  color1: '#3022bb', // blue,
+  color2: '#6f68ae', // lightBlue,
+  color3: '#f989ff', // lightPink,
+  color4: '#730d61', // darkPink,
+  color5: '#080707', // black,
+  color6: '#2e3f41', // grey,
+  color7: '#f7da8b', // skinYellow,
+  color8: '#ffffff', // white,
+  color9: 'rgba(255, 255, 255, .0)',
+}
+const theme = { colors }
+// <script type="text/javascript" src="https://embed.typeform.com/embed.js"></script>
+//     <iframe id="typeform-full" width="100%" height="100%" frameborder="0" src="https://ninabreznik.typeform.com/to/RnddyU"></iframe>
+const data = {
+  title: 'Taipei',
+  logo: 'assets/wizard3.png',
+  home: 'http://wizardamigos.com/',
+  tabs: [{
+    title: 'wizardamigos',
+    url: 'https://wizardamigos.com/'
+  },{
+    title: 'hangout',
+    url: 'https://meet.jit.si/wizardamigos-taipei'
+  }, {
+    title: 'CodeCamp2018',
+    url: 'https://wizardamigos-codecamp2018.herokuapp.com/landingPage'
+  }],
+  chat: 'https://gitter.im/wizardamigosinstitute/program/~embed',
+  chapters: [{
+    title: 'Berlin',
+    url: 'http://wizardamigos.com/chapter-berlin/',
+  },{
+    title: 'Nashville',
+    url: 'http://wizardamigos.com/chapter-nashville/',
+  }]
+}
+
+const { title, logo, home, tabs, chat, chapters } = data
+document.body.innerHTML = `<h1> Loading data - please wait ... </h1>`
+var meetup_api = 'https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=WizardAmigos&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=35240262&sig=0ec0705819f4127f8d9396d29db27c58b3f1bb50'
+var url = 'https://cors-anywhere.herokuapp.com/' + meetup_api
+try {
+  var { place, time, event } = JSON.parse(localStorage.lastEvent)
+  var old = new Date(time)
+  var now = new Date()
+  var timeDiff = now.getTime() - old.getTime()
+  var diffDays = timeDiff / (1000 * 3600 * 24)
+  if (diffDays < 0) throw new Error('cached event is over')
+  data.place = place
+  data.time = time
+  data.event = event
+  init(data)
+} catch (e) {
+  fetch(url)
+  .then(response => response.json())
+  .then(res => {
+    for (var i = 0; i < res.results.length; i++) {
+      var place = res.results[i].venue.city
+      if (place === title) {
+        var event = res.results[i].event_url
+        var number = res.results[i].time
+        var time = new Date(number).toString()
+        break
+      }
     }
-    *,*:before,*:after { box-sizing  : inherit; }
-    @font-face    {
-      font-family : C64;
-      src         : url(assets/C64_Pro-STYLE.woff);
-    }
-    body {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      margin: 0;
-      height: 100vh;
-      width: 100vw;
-      background-color: #43409a;
-    }
-    h3 {
-      color: ${W};
-    }
-    iframe {
-      border: 10px solid ${W};
-      width: 100%;
-      height: 100%;
-      flex-grow: 1;
-    }
-    .content {
-      display: flex;
-      width: 100%;
-      height: 100%;
-    }
-    .input {
-      width: 100%;
-    }
-    .address {
-      width: 400px;
-    }
-    a {
-      color: inherit;
-      text-decoration: none;
-      cursor: pointer;
-    }
-    .header {
-      display: flex;
-      flex-direction: row;
-      justify-content: flex-start;
-      align-items: center;
-      width: 100vw;
-      height: 220px;
-    }
-    .logo {
-      margin-left: 20px;
-      margin-top: 10px;
-      width: 120px;
-      height: 120px;
-    }
-    .title {
-      color: ${W};
-      font-family: C64;
-      margin-left: 10vw;
-      font-size: 30px;
-    }
-    .title > span {
-      color: ${lP};
-    }
-    .webring {
-      text-decoration: underline;
-      color: ${dP};
-      font-family: C64;
-      font-size: 30px;
-    }
-    .chapter {
-      color: ${lP};
-      font-family: C64;
-      font-size: 15px;
-    }
-    .chapter-list {
-      list-style-image: url(assets/wai-icon1.png)
-    }
-  </style>
-`
-var body = `
-  <div class="header">
-  <a href="http://wizardamigos.com/"><img class="logo" src="assets/wizard3.png"></a>
-  <h1 class="title">WizardAmigos Chapter: <span>Taipei</span></h1>
-  </div>
-  <div class="content">
-    <iframe id="typeform-full" width="100%" height="100%" frameborder="0" src="https://wizardamigos-codecamp2018.herokuapp.com/landingPage"></iframe>
-    <iframe class="iframe" src="https://gitter.im/wizardamigosinstitute/program/~embed"></iframe>
-  </div>
-  <h3 class="webring"> other chapters </h3>
-  <ul class="chapter-list"><li class="chapter"><a href="http://wizardamigos.com/chapter-berlin/">Berlin</a></li></ul>
-  <script type="text/javascript" src="https://embed.typeform.com/embed.js"></script>
-`
-document.head.innerHTML = head
-document.body.innerHTML = body
+    localStorage.lastEvent = JSON.stringify({ place, event, time })
+    data.place = place
+    data.time = time
+    data.event = event
+    init(data)
+  })
+}
+function init (data) {
+  const {
+    head,
+    body,
+  } = chapterpage({ data, theme })
+  document.head.innerHTML = head
+  document.body.innerHTML = body
+}
